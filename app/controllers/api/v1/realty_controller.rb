996cb38ea2f99c893f933fd4a4351f39f48f09d1
esarayaj <esteban.araya2@walmart.com>
2021-03-06 18:26:29 -0300

    remove slqlite and add api image

--- app/controllers/api/v1/realty_controller.rb
+++ app/controllers/api/v1/realty_controller.rb
@@ -1,10 +1,28 @@
 module Api
     module V1
       class RealtyController < ApplicationController
+        before_action :set_realty, only:  [:show,:images]
+
         def index
           @realty = Realty.all
           render json: @realty
         end
+
+        def images
+          @fileUpload = FileUpload.find_by(id: @realty.id)
+          render json: @fileUpload
+        end
+
+        def show
+          render json: @realty
+        end
+
+        private
+
+        def set_realty
+          @realty = Realty.find(params[:id])
+        end
+      
       end
     end
 end
