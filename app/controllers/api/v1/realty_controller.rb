d00667b9c119c9e9504ed24083dd349720b56446
esarayaj <esteban.araya2@walmart.com>
2021-04-06 23:21:33 -0400

    refactor column API

--- app/controllers/api/v1/realty_controller.rb
+++ app/controllers/api/v1/realty_controller.rb
@@ -1,21 +1,61 @@
 module Api
     module V1
       class RealtyController < ApplicationController
+        include Rails.application.routes.url_helpers
+
         #before_action :authenticate_request!
         before_action :set_realty, only:  [:show,:images]
 
         def index
-          @realty = Realty.select('*,realties.id as id,communes.name as commune,provinces.name as provinces,regions.name as regions')
+          @realty = Realty.select("
+            realties.id,
+            CONCAT(coalesce(apple, 0),coalesce(property, 0)) as avaluo,
+            name_realty,
+            CONCAT(unit_estate,' ', street,' ', number_unit,' ', communes.name) as address,
+            coalesce(mt2_land, 0) as mt2_land,
+            coalesce(mt2_built, 0) as mt2_built,
+            coalesce(bathroom, 0) as bathroom,
+            coalesce(parking_lot, 0) as parking_lot,
+            coalesce(cellar, 0) as cellar,
+            coalesce(bedroom, 0) as bedroom,
+            coalesce(ambient, 0) as ambient,
+            latitude,
+            longitude,
+            case WHEN coalesce(rent, 0) = 1  THEN 'publicado' else 'no publicado' end as rent,
+            case WHEN coalesce(sell, 0) = 1  THEN 'publicado' else 'no publicado' end as sell,
+            case WHEN coalesce(rent_season, 0) = 1  THEN 'publicado' else 'no publicado' end as rent_season,
+            case WHEN coalesce(rent_state, 0) = 1  THEN 'valido arrendar' else 'no valido arrendar' end as rent_state,
+            case WHEN coalesce(sell_state, 0) = 1  THEN 'valido vender' else 'no valido vender' end as sell_state,
+            case WHEN tipo = 0  THEN 'casa'
+            WHEN tipo = 1  THEN 'departamento'
+            WHEN tipo = 2  THEN 'local'
+            WHEN tipo = 3  THEN 'oficina'
+            WHEN tipo = 4  THEN 'bodega'
+            WHEN tipo = 5  THEN 'sitio'
+            WHEN tipo = 6  THEN 'estacionamiento'
+            WHEN tipo = 7  THEN 'parcela'
+            end as tipo,
+            communes.name as commune, regions.name as region")
+          .joins("INNER JOIN type_realties ON type_realties.realty_id = realties.ID")
           .joins("INNER JOIN communes ON communes.id = realties.commune_id")
-          .joins("INNER JOIN provinces ON provinces.id = communes.province_id")
           .joins("INNER JOIN regions ON regions.id = communes.region_id")
           .joins("LEFT JOIN publications ON publications.realty_id = realties.id").order("realties.id")
-
           render json: @realty.paginate(page: params[:page], per_page: 100)
         end
 
         def images
-          @fileUpload = FileUpload.where(model_id: @realty.id, model: "realty")
+          @url = request.base_url
+          @fileUpload = FileUpload.select("
+            id ,
+            CONCAT('"+@url+"','/uploads/',file_name) as file_name,
+            model ,
+            model_id,
+            created_at ,
+            updated_at ,
+            coalesce(title, '') as title ,
+            coalesce(description, '') as description,
+            coalesce(original_file_name, '') as original_file_name,
+            case WHEN coalesce(status, 0) = 1  THEN 'publicado' else 'no publicado' end as status").where(model_id: @realty.id, model: "realty")
           render json: @fileUpload ? @fileUpload : []
         end
 
